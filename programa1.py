beecb193cb3d54debf7555ccea9ff89b8ac8dd92
MC Gutierrez-Diez <cgutierr@uach.mx>
2025-05-28 17:36:56 -0600

    Update programa1.py

--- programa1.py
+++ programa1.py
@@ -7,15 +7,12 @@ nombre = st.text_input("Escribe tu nombre")
 
 if nombre:
     st.write(f"Hola, {nombre} 👋")
+    
+# Seleccion multiple
+options = st.multiselect(
+    "Cuales son tus dudas de hoy?",
+    ["Proposiciones logicas", "Conjuntos", "Grafos"],
+    default=["Proposiciones logicas"],
+)
 
-# Calendario
-import datetime
-st.title("Ejemplo de fecha")
-# Widget: date_input
-fecha = st.date_input("Selecciona una fecha para asesoria", value=datetime.date.today())
-st.write("La fecha seleccionada es:", fecha)
-
-st.title("Ejemplo de Checkbox")
-l=True)
-if st.button("click"):
-    st.balloons()
+st.write("Seleccionaste:", options)
