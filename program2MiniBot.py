cad5c079de0f4853dda7195c3b517b65c37d94ce
MC Gutierrez-Diez <cgutierr@uach.mx>
2025-05-29 16:06:56 -0600

    Update program2MiniBot.py

--- program2MiniBot.py
+++ program2MiniBot.py
@@ -12,8 +12,8 @@ prompt = st.chat_input("¿Hola, en que te puedo ayudar hoy?")
 if prompt==None:
    st.stop()
 
-#with st.chat_message("user"):
-#   st.markdown(prompt)
+with st.chat_message("user", avatar = "😊"):
+   st.markdown(prompt)
 
 # Generate a response using the OpenAI API.
 
